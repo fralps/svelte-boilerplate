e00e4dc0acadbdf20b85d30ca515e39fb3c566c4
fralps <francois@kinoba.fr>
2024-02-23 16:55:51 +0100

    chore: add stylelint, eslint and prettier packages + config

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import svelteLogo from '@assets/svelte.svg'
-  import viteLogo from '/vite.svg'
+  import svelteLogo from '@assets/svelte.svg';
+  import viteLogo from '/vite.svg';
 </script>
 
 <main>
@@ -15,12 +15,11 @@
   <h1>Vite + Svelte</h1>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the
+    official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
@@ -30,12 +29,15 @@
     will-change: filter;
     transition: filter 300ms;
   }
+
   .logo:hover {
     filter: drop-shadow(0 0 2em #646cffaa);
   }
+
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
   }
+
   .read-the-docs {
     color: #888;
   }
